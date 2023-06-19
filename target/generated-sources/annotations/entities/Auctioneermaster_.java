9576226c9acb91b48bad42e92860657eea9f1045
raj332 <praj48848@gmail.com>
2023-06-20 01:21:32 +0530

    raj-20-6

--- target/generated-sources/annotations/entities/Auctioneermaster_.java
+++ target/generated-sources/annotations/entities/Auctioneermaster_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-06-19T22:32:20")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-06-20T00:53:54")
 @StaticMetamodel(Auctioneermaster.class)
 public class Auctioneermaster_ { 
 
--- target/generated-sources/annotations/entities/Soldplayertb_.java
+++ target/generated-sources/annotations/entities/Soldplayertb_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-06-19T22:32:20")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-06-20T00:53:54")
 @StaticMetamodel(Soldplayertb.class)
 public class Soldplayertb_ { 
 
